21d114ddbc7682153aad821a4b99ae1f6fe98426
PedroAz <pdegutty@gmail.com>
2023-05-12 14:40:31 -0300

    form

--- src/main/java/com/app/boardgames/controller/IndexController.java
+++ src/main/java/com/app/boardgames/controller/IndexController.java
@@ -3,26 +3,15 @@ package com.app.boardgames.controller;
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
-
 import javax.transaction.Transactional;
-import javax.validation.Valid;
-
-import com.app.boardgames.dto.RequisicaoNovoJogo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-
 import com.app.boardgames.model.DadosJogo;
 import com.app.boardgames.repository.DadosJogosRepository;
 
